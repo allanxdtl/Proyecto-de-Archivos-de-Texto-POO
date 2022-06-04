a751d436c0ef75e740add5aa9ff6866d565b6fb0
Bryan Valdez <alan272_@hotmail.com>
2022-06-03 22:33:00 -0700

    Añadida nueva funcion [Añadir producto al menú]

--- Proyecto/ModProducForm.Designer.cs
+++ Proyecto/ModProducForm.Designer.cs
@@ -29,19 +29,169 @@ namespace Proyecto
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bttnCerrarMod = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Modificar producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(288, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Escoga el producto que desea modificar";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Precio});
+            this.dataGridView1.Location = new System.Drawing.Point(26, 168);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(397, 245);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 104;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            this.Precio.Width = 85;
+            // 
+            // bttnCerrarMod
+            // 
+            this.bttnCerrarMod.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.bttnCerrarMod.FlatAppearance.BorderSize = 2;
+            this.bttnCerrarMod.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.bttnCerrarMod.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.bttnCerrarMod.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnCerrarMod.Location = new System.Drawing.Point(252, 437);
+            this.bttnCerrarMod.Name = "bttnCerrarMod";
+            this.bttnCerrarMod.Size = new System.Drawing.Size(159, 37);
+            this.bttnCerrarMod.TabIndex = 14;
+            this.bttnCerrarMod.Text = "Cerrar Ventana";
+            this.bttnCerrarMod.UseVisualStyleBackColor = true;
+            this.bttnCerrarMod.Click += new System.EventHandler(this.bttnCerrarMod_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(29, 437);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 37);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Modificar Producto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(340, 104);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(54, 27);
+            this.txtPrecio.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(281, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 19);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Precio";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(93, 104);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(170, 27);
+            this.txtNombre.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 19);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Nombre";
             // 
             // ModProducForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(297, 289);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(436, 497);
+            this.ControlBox = false;
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bttnCerrarMod);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Maiandra GD", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "ModProducForm";
             this.Text = "ModProducForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.Button bttnCerrarMod;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Proyecto/ModProducForm.cs
+++ Proyecto/ModProducForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Proyecto
 {
@@ -15,6 +16,106 @@ namespace Proyecto
         public ModProducForm()
         {
             InitializeComponent();
+            try
+            {
+                StreamReader reader = File.OpenText("ArchivosTexto/menu.txt");
+                int indice = 0;
+                bool noleido = true;
+                string linea;
+                string[] separados;
+                while (noleido)
+                {
+                    try
+                    {
+                        indice = dataGridView1.Rows.Add();
+                        linea = reader.ReadLine();
+                        productos.Add(linea);
+                        separados = linea.Split(',');
+                        dataGridView1.Rows[indice].Cells[0].Value = separados[0];
+                        dataGridView1.Rows[indice].Cells[1].Value = separados[1];
+                    }
+                    catch
+                    {
+                        noleido = false;
+                        reader.Close();
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("El archivo no ha sido encontrado");
+            }
+        }
+
+        List<string> productos = new List<string>();
+
+        private void bttnCerrarMod_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            productos[dataGridView1.SelectedRows[0].Index] = $"{txtNombre.Text}, {txtPrecio.Text}";
+            try
+            {
+                StreamWriter writer = File.CreateText("ArchivosTexto/menu.txt");
+                for(int i=0; i<productos.Count; i++)
+                {
+                    writer.WriteLine(productos[i]);
+                }
+                writer.Close();
+                MessageBox.Show("El producto ha sido modidicado con exito!","Ya esta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView1.Rows.Clear();
+                try
+                {
+                    StreamReader reader = File.OpenText("ArchivosTexto/menu.txt");
+                    int indice = 0;
+                    bool noleido = true;
+                    string linea;
+                    string[] separados;
+                    while (noleido)
+                    {
+                        try
+                        {
+                            indice = dataGridView1.Rows.Add();
+                            linea = reader.ReadLine();
+                            productos.Add(linea);
+                            separados = linea.Split(',');
+                            dataGridView1.Rows[indice].Cells[0].Value = separados[0];
+                            dataGridView1.Rows[indice].Cells[1].Value = separados[1];
+                        }
+                        catch
+                        {
+                            noleido = false;
+                            reader.Close();
+                        }
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    MessageBox.Show("El archivo no ha sido encontrado");
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Ocurrio un error");
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txtNombre.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                txtPrecio.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Por favor seleccione el renglon completo", "Ojo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            
         }
     }
 }
